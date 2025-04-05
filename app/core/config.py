db24d074917b89b10609529c17399377746af096
Ayush56565 <ayush.sengupta11@gmail.com>
2025-04-05 23:32:27 +0530

    database config changes

--- app/core/config.py
+++ app/core/config.py
@@ -7,17 +7,11 @@ load_dotenv()
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Clips Service"
-
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "postgres")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "clips_db")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", "5432")
     DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
-        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"{self.DATABASE_URL}"
     
     class Config:
         case_sensitive = True
