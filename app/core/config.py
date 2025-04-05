01add6844587b4f7a01e7fe47312ea229143b34a
Ayush56565 <ayush.sengupta11@gmail.com>
2025-04-05 23:34:34 +0530

    changed database config

--- app/core/config.py
+++ app/core/config.py
@@ -8,7 +8,6 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "Clips Service"
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
         return f"{self.DATABASE_URL}"
